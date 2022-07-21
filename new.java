006bed5c10295dc314928aa36c3ec2f8ab26652d
Msaritha7 <105190279+Msaritha7@users.noreply.github.com>
2022-07-21 07:06:11 -0700

    done changes

--- new.java
+++ new.java
@@ -5,5 +5,6 @@ public static void main(String args[])){
     System.out.println("firstjava");
     System.out.println("x");
     System.out.println("y");
+  System.out.println("done my changes");
   }
 }
